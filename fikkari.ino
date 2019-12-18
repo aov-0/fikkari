bd1b0879f2e8dfe9ea170891b8d9bea3a39507cc
Aku Vilkki <aku.vilkki@ictpro.labra>
2019-12-18 15:05:16 +0200

    toinen uppaus

--- fikkari.ino
+++ fikkari.ino
@@ -1,10 +1,12 @@
 #include "fikkari.h"
 
 fikkari T1(5, 6); //Luodaan ilmentymä meidän luokasta
+fikkari T1(4, 3); //Luodaan ilmentymä meidän luokasta
  
 void setup() {
 }
 
 void loop() {
   T1.handle();
+  T2.handle();
 }
